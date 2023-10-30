7619f6059a90734d005e490f80fbbf0c55603794
Siwoo Lee <siwoo28@kakao.com>
2023-10-30 14:18:10 +0900

    Style: detail layout

--- 01.javascript/todoapp/assets/js/pages/info/TodoInfo.js
+++ 01.javascript/todoapp/assets/js/pages/info/TodoInfo.js
@@ -1,26 +1,32 @@
 // 할일 등록
-import Header from '../../layout/Header.js';
-import Footer from '../../layout/Footer.js';
+import Header from "../../layout/Header.js";
+import Footer from "../../layout/Footer.js";
 
-const TodoInfo = async function({_id} = {}){
-  const page = document.createElement('div');
-  page.setAttribute('id', 'page');
+const TodoInfo = async function ({ _id } = {}) {
+  const page = document.createElement("div");
+  page.setAttribute("id", "page");
 
-  const BASE_URL = 'http://localhost:33088';
-  let response = await axios(`${ BASE_URL }/api/todolist/${_id}`)
-  console.log(response)
-  const data = response.data.item
+  const BASE_URL = "http://localhost:33088";
+  let response = await axios(`${BASE_URL}/api/todolist/${_id}`);
+  console.log(response);
+  const data = response.data.item;
 
-  const content = document.createElement('div');
-  console.log(data)
+  const content = document.createElement("div");
+  console.log(data);
 
-  for (let key in data){
-    let item = data[key]
+  const ul = document.createElement("ul");
+  ul.setAttribute("class", "todolist");
+  for (let key in data) {
+    let item = data[key];
+    const li = document.createElement("li");
     const text = document.createTextNode(`${item}`);
-    content.appendChild(text)
-  }
 
-  page.appendChild(Header('TODO App 상세 조회'));
+    li.appendChild(text);
+    ul.appendChild(li);
+  }
+  content.appendChild(ul);
+
+  page.appendChild(Header("TODO App 상세 조회"));
   page.appendChild(content);
   page.appendChild(Footer());
 
