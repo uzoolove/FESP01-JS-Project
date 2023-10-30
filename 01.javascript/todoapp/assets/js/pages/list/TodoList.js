c6db404bbc74c9555f9fb86440213f71bb7b9859
junyeoplee <jyeop920@gmail.com>
2023-10-30 16:51:01 +0900

    [Style]: 투두리스트 스타일 수정 및 코드 줄 정리

--- 01.javascript/todoapp/assets/js/pages/list/TodoList.js
+++ 01.javascript/todoapp/assets/js/pages/list/TodoList.js
@@ -5,67 +5,95 @@ import TodoRegist from '../regist/TodoRegist.js';
 import TodoInfo from '../info/TodoInfo.js';
 
 const TodoList = async function(){
+  //NOTE -base url 상수로 지정
   const BASE_URL = 'http://localhost:33088';
+
+  //NOTE - 투두 컨테이너
   const page = document.createElement('div');
   page.setAttribute('id', 'page');
 
+  //NOTE - 버튼 담는 박스
+  const registButtonBox = document.createElement('div');
+  
+  //NOTE - 투두 내용 담는 박스 
   const content = document.createElement('div');
   content.setAttribute('id', 'content');
-  let response;
-  try{
-    response = await axios(`${ BASE_URL }/api/todolist`)
-
+  
+  try {
+    const response = await axios(`${ BASE_URL }/api/todolist`)
+    
     const ul = document.createElement('ul');
     ul.setAttribute('class', 'todolist');
+
     response.data?.items.forEach(item => {
       const li = document.createElement('li');
 
+      /**
+       * //NOTE - 체크 박스 누르면 완료 표시 되도록 patch로 요청 보내고
+       * //NOTE - 게시물의 done 프로퍼티의 값이 true 거나 false일때 완료 표시 혹은 표시 x
+       */
       const checkbox = document.createElement('input');
+      const checkboxLable = document.createElement('label');
+      checkboxLable.setAttribute('for', `checkbox_${item._id}`);
       checkbox.setAttribute('type', 'checkbox');
       checkbox.setAttribute('id', `checkbox_${item._id}`);
       checkbox.checked = item.done;
 
+      if (item.done) {
+        li.style.textDecoration = 'line-through';
+      }
+
       checkbox.addEventListener('change', async function (event) {
         const isChecked = checkbox.checked;
+
         try {
           // API 업데이트 요청 보내기
-          await axios.patch(`http://localhost:33088/api/todolist/${item._id}`, { done: isChecked });
+          await axios.patch(
+            `http://localhost:33088/api/todolist/${item._id}`, { done: isChecked });
+
           item.done = isChecked;
-          console.log(item);
+
           if (isChecked) {
             item.done = false;
-          } else {
+            event.target.parentNode.style.textDecoration = 'line-through';
+          } 
+          else {
             item.done = true;
+            event.target.parentNode.style.textDecoration = 'none';
           }
         } catch (error) {
           console.error('API 업데이트에 실패했습니다:', error);
         }
       });
 
+      //NOTE - 게시물 상세 페이지로 이동할 수 있는 link 이벤트
       const todoInfoLink = document.createElement('a');
       todoInfoLink.setAttribute('href', `info?_id=${item._id}`);
+
       const title = document.createTextNode(item.title);
 
       todoInfoLink.addEventListener('click', async function (event) {
-        // 브라우저의 기본 동작 취소(<a> 태그 동작 안하도록)
         event.preventDefault();
         const infoPage = await TodoInfo({ _id: item._id });
         document.querySelector('#page').replaceWith(infoPage);
       });
 
       li.appendChild(checkbox);
-      li.appendChild(title);
+      li.appendChild(checkboxLable);
       li.appendChild(todoInfoLink);
+      todoInfoLink.appendChild(title);
 
       ul.appendChild(li);
     });
+
     content.appendChild(ul);
 
     const btnRegist = document.createElement('button');
-    const btnTitle = document.createTextNode('등록');
+    const btnTitle = document.createTextNode('+');
     btnRegist.appendChild(btnTitle);
-    content.appendChild(btnRegist);
+    registButtonBox.appendChild(btnRegist);
 
+    //NOTE - 버튼 클릭 하면 글 등록 페이지로 이동
     btnRegist.addEventListener('click', () => {
       const registPage = TodoRegist();
       document.querySelector('#page').replaceWith(registPage);
@@ -75,7 +103,8 @@ const TodoList = async function(){
     content.appendChild(error);
   }
 
-  page.appendChild(Header('TODO App 목록 조회'));
+  page.appendChild(Header('TodoList'));
+  page.appendChild(registButtonBox);
   page.appendChild(content);
   page.appendChild(Footer());
   return page;
