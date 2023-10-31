dd30eea92f2e4a560461f4a55b898667f6dbab3c
minkyeongJ <jomk2221@naver.com>
2023-10-31 13:15:30 +0900

    Feat: 할 일 내용 수정 기능 추가

--- 01.javascript/todoapp/assets/js/pages/update/TodoUpdate.js
+++ 01.javascript/todoapp/assets/js/pages/update/TodoUpdate.js
@@ -2,7 +2,104 @@
 import Header from "../../layout/Header.js";
 import Footer from "../../layout/Footer.js";
 
-const TodoUpdate = () => {
-  return;
+import isEmpty from "../../utils/isEmpty.js";
+import update from "../../apis/update.js";
+
+const TodoUpdate = async () => {
+  //주소에서 id 가져오기
+  const urlStr = window.location.href;
+  const url = new URL(urlStr);
+  const urlparams = url.searchParams;
+  const ID = urlparams.get("_id");
+
+  const response = await axios.get(`http://localhost:33088/api/todolist/${ID}`);
+
+  const item = response.data.item;
+
+  const page = document.createElement("div");
+  page.setAttribute("id", "page");
+
+  // 제목
+  const titleDiv = document.createElement("div");
+  titleDiv.textContent = "할 일 :";
+  const titleInput = document.createElement("input");
+  titleInput.setAttribute("id", "title-create");
+  titleInput.setAttribute("name", "title-create");
+  titleInput.setAttribute("placeholder", "할 일을 입력하세요.");
+  titleInput.setAttribute("value", item.title);
+  page.appendChild(titleDiv);
+  titleDiv.appendChild(titleInput);
+
+  // 상세 내용
+  const contentDiv = document.createElement("div");
+  contentDiv.textContent = "상세 내용 : ";
+  const contentInput = document.createElement("textarea");
+  contentInput.setAttribute("id", "content-create");
+  contentInput.setAttribute("name", "content-create");
+  contentInput.setAttribute("placeholder", "상세 내용을 입력하세요.");
+  contentInput.textContent = item.content;
+  page.appendChild(contentDiv);
+  contentDiv.appendChild(contentInput);
+
+  // 체크박스
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("id", "checkbox");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("name", "checkbox");
+  checkbox.setAttribute("checked", false);
+  checkbox.checked = item.done;
+  page.appendChild(checkbox);
+
+  // 수정 완료 버튼 - confirm
+  const submit = document.createElement("input");
+  submit.setAttribute("type", "submit");
+  submit.setAttribute("value", "수정 완료");
+  submit.addEventListener("click", handleUpdate);
+
+  page.appendChild(submit);
+
+  function handleUpdate(e) {
+    e.preventDefault();
+
+    const titleValue = document.querySelector("#title-create").value;
+    const contentValue = document.querySelector("#content-create").value;
+
+    //입력값 확인(이중확인)
+    if (titleValue === "") {
+      alert("제목을 입력하세요");
+    }
+    if (contentValue === "") {
+      alert("상세 내용을 입력하세요");
+    }
+    if (confirm("할 일을 등록하시겠습니까?")) {
+      // axios post
+      update(item._id, titleInput.value, contentInput.value, checkbox.checked);
+    }
+  }
+
+  function checkButtonStatus() {
+    const titleValue = titleInput.value;
+    const contentValue = contentInput.value;
+
+    if (titleValue === "" || contentValue === "") {
+      submit.setAttribute("disabled", true);
+    } else {
+      submit.removeAttribute("disabled");
+    }
+  }
+
+  titleInput.addEventListener("input", checkButtonStatus);
+
+  // 취소 버튼 (이전페이지)
+  const cancel = document.createElement("button");
+  cancel.innerText = "취소";
+  cancel.addEventListener("click", () => {
+    window.history.back();
+  });
+  page.appendChild(cancel);
+
+  // 헤터, 푸터 추가
+
+  return page;
 };
 export default TodoUpdate;
