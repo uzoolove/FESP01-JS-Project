8267df5779420c54c9a7790ee8b5fa1a66e4a32a
LKJ970524 <dlrbwjd7096@naver.com>
2023-11-06 12:38:18 +0900

    [Refactor] 리펙토링 - JavaScript 에서 TypeScript로 리펙토링 작업 완료

--- 02.typescript/todoapp/src/api/todos.api.ts
+++ 02.typescript/todoapp/src/api/todos.api.ts
@@ -1,41 +1,16 @@
 import { linkTo } from "../Router";
-<<<<<<< HEAD
-import axios, { AxiosResponse } from "axios";
-
-export interface Todo {
-  _id: string;
-  title: string;
-  content: string;
-  done: boolean;
-}
-
-interface TodoListResponse {
-  data: Todo[];
-}
-
-const BASE_URL = "http://localhost:33088";
-
-export const getTodoList = async (): Promise<AxiosResponse<TodoListResponse>> => {
-  const response = await axios.get<TodoListResponse>(`${BASE_URL}/api/todolist`);
-=======
-import axios from "../../node_modules/axios/index";
+import axios from "axios";
 
 const BASE_URL = "http://localhost:33088";
 
 //NOTE - 할일 목록을 가져오는 함수
 export const getTodoList = async () => {
-  const response = 
-  await axios<TodoListResponse>(`${ BASE_URL }/api/todolist`);
->>>>>>> 04b8bbb3c18cb09f6494dd2637ed1ab1e8fe61b0
+  const response = await axios<TodoListResponse>(`${BASE_URL}/api/todolist`);
   return response;
 };
 
 //NOTE - 할일 등록을 처리하는 함수
-<<<<<<< HEAD
-export const onSubmitTodoRegister = async (event: Event, titleInput: HTMLInputElement, contentInput: HTMLTextAreaElement) => {
-=======
 export const onSubmitTodoRegister = async (event: any, titleInput: any, contentInput: any) => {
->>>>>>> 04b8bbb3c18cb09f6494dd2637ed1ab1e8fe61b0
   event.preventDefault();
 
   if (titleInput.value === "" || contentInput.value === "") {
@@ -51,8 +26,7 @@ export const onSubmitTodoRegister = async (event: any, titleInput: any, contentI
 
   try {
     if (confirm("등록 하시겠습니까?")) {
-      const response = 
-      await axios.post(`${ BASE_URL }/api/todolist`, body);
+      const response = await axios.post(`${BASE_URL}/api/todolist`, body);
 
       if (response) {
         linkTo("/");
@@ -64,37 +38,24 @@ export const onSubmitTodoRegister = async (event: any, titleInput: any, contentI
 };
 
 //NOTE - 체크박스 상태 변경을 처리하는 함수
-<<<<<<< HEAD
-export const onChangeCheckbox = async (event: Event, checkbox: HTMLInputElement, todo: Todo) => {
+export const onChangeCheckbox = async (event: any, checkbox: any, todo: any) => {
   console.log(typeof event, typeof checkbox, typeof todo);
   const isChecked = checkbox.checked;
-=======
-export const onChangeCheckbox = async (
-  event: Event, checkbox: HTMLInputElement, todo: TodoItem): Promise<void> => {
-
-    const isChecked: boolean = checkbox.checked;
->>>>>>> 04b8bbb3c18cb09f6494dd2637ed1ab1e8fe61b0
 
   try {
-    const response = 
-    await axios.patch(`${ BASE_URL }/api/todolist/${ todo._id }`, { done: isChecked });
+    const response = await axios.patch(`${BASE_URL}/api/todolist/${todo._id}`, { done: isChecked });
 
     if (response) {
-<<<<<<< HEAD
-      const target = event.target as HTMLInputElement;
-      const NEXT_SIBLING = target.nextSibling as HTMLElement;
-=======
-      const NEXT_SIBLING = (event.target as HTMLElement)?.nextSibling;
->>>>>>> 04b8bbb3c18cb09f6494dd2637ed1ab1e8fe61b0
+      const NEXT_SIBLING = event.target.nextSibling;
 
       todo.done = isChecked;
 
       if (isChecked) {
         todo.done = false;
-        (NEXT_SIBLING as HTMLElement)?.classList.add("line-through");
+        NEXT_SIBLING.classList.add("line-through");
       } else {
         todo.done = true;
-        (NEXT_SIBLING as HTMLElement)?.classList.remove("line-through");
+        NEXT_SIBLING.classList.remove("line-through");
       }
     }
   } catch (error) {
@@ -103,18 +64,11 @@ export const onChangeCheckbox = async (
 };
 
 //NOTE - TodoList 화면에서 할일 삭제를 처리하는 함수
-<<<<<<< HEAD
-export const onClickDeleteTodo = async (todoId: string, li: HTMLLIElement) => {
+export const onClickDeleteTodo = async (todoId: any, li: any) => {
   console.log(typeof todoId, typeof li);
-=======
-export const onClickDeleteTodo = async (
-  todoId: number, li: HTMLElement): Promise<void> => {
-
->>>>>>> 04b8bbb3c18cb09f6494dd2637ed1ab1e8fe61b0
   try {
     if (confirm("정말 삭제 하시겠습니까?")) {
-      const response = 
-      await axios.delete<TodoListResponse>(`${ BASE_URL }/api/todolist/${ todoId }`);
+      const response = await axios.delete(`${BASE_URL}/api/todolist/${todoId}`);
 
       if (response) {
         li.remove();
@@ -154,11 +108,7 @@ export const onClickDeleteInInfo = async (todoId: string) => {
 };
 
 //NOTE - 할일 수정을 처리하는 함수
-<<<<<<< HEAD
-export const onClickEditTodo = async (event: Event, titleInput: HTMLInputElement, contentInput: HTMLTextAreaElement, todoId: string) => {
-=======
 export const onClickEditTodo = async (event: any, titleInput: any, contentInput: any, todoId: any, backButton: any) => {
->>>>>>> 04b8bbb3c18cb09f6494dd2637ed1ab1e8fe61b0
   event.preventDefault();
 
   if (titleInput.value === "" || contentInput.value === "") {
@@ -182,7 +132,7 @@ export const onClickEditTodo = async (event: any, titleInput: any, contentInput:
       const response = await axios.patch(`${BASE_URL}/api/todolist/${todoId}`, body);
 
       if (response) {
-        window.history.back();
+        linkTo(backButton.getAttribute("href"));
       }
     }
   } catch (error) {
