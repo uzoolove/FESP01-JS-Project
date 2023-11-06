cf4770b6af9af2060d25cfc6500410e9da5749d9
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-11-06 14:22:47 +0900

    [Refactor] 상세 조회 및 수정 JavaScript 에서 TypeScript로 리펙토링

--- 02.typescript/todoapp/src/pages/update/TodoUpdate.ts
+++ 02.typescript/todoapp/src/pages/update/TodoUpdate.ts
@@ -3,12 +3,10 @@ import Footer from '../../layout/Footer';
 import { linkTo } from '../../Router';
 import { getTodoData, onClickEditTodo } from '../../api/todos.api';
 
-
-
 const TodoUpdate = async () => {
   //NOTE - URL 매개변수에서 _id 값 추출
   const params = new URLSearchParams(location.search);
-  const _id = params.get('_id');
+  const _id = params.get('_id')!;
 
   //NOTE - 특정 할일의 정보 가져오기
   const todoDetail = await getTodoData(_id);
@@ -30,14 +28,14 @@ const TodoUpdate = async () => {
   //NOTE - 요소에 속성 및 클래스 추가
   sumbmitForm.setAttribute('id', 'regist-form');
   titleInput.setAttribute('type', 'text');
-  titleInput.setAttribute('maxLength', 24);
+  titleInput.setAttribute('maxLength', '24');
   titleInput.setAttribute('value', todoDetail.title);
   contentInput.textContent = todoDetail.content;
 
   ButtonBox.setAttribute('class', 'button-area');
   backButton.setAttribute('type', 'button');
   backButton.setAttribute('class', 'back-button');
-  backButton.setAttribute('href', `info?_id=${ _id }`);
+  backButton.setAttribute('href', `info?_id=${_id}`);
 
   submitEditButton.setAttribute('class', 'submit-button');
   submitEditButton.setAttribute('type', 'submit');
@@ -58,13 +56,13 @@ const TodoUpdate = async () => {
 
   //NOTE - 폼 제출 이벤트 리스너 추가
   sumbmitForm.addEventListener('submit', (event) => {
-    onClickEditTodo(event, titleInput, contentInput, _id, backButton);
+    onClickEditTodo(event, titleInput, contentInput, _id);
   });
 
   //NOTE - 취소 버튼 클릭 시 이벤트 설정
   backButton.addEventListener('click', () => {
     if (confirm('취소 하고 상세 페이지로 이동합니다')) {
-      linkTo(backButton.getAttribute('href'));
+      linkTo(backButton.getAttribute('href')!);
     }
   });
 
