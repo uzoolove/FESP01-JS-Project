3239adda323add74795879691c3b8a9a7d30d925
junyeoplee <jyeop920@gmail.com>
2023-11-10 13:27:27 +0900

    [Add]: 초기 환경 셋팅

---
+++ 03.react-typescript/src/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        FESP 1기 Javascript Project
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ 03.react-typescript/src/page/TodoRegist.tsx
@@ -0,0 +1,11 @@
+import Header from "src/layout/header/Header";
+
+const TodoRegist = () => {
+ return (
+  <>
+   <Header title={"등록 페이지"} />
+  </>
+ );
+};
+
+export default TodoRegist;
